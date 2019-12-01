918ed8faf2e6fa6a7f282c95bb1240d5431a6b26
Tim C <47341920+goodSirTimothy@users.noreply.github.com>
2019-11-30 20:35:00 -0500

    Basic Update (#5)
    * added basic dispatcher and servlet logic

---
+++ Poof-O-Floof/src/main/java/model/AnimalBasic.java
@@ -0,0 +1,12 @@
+package model;
+
+public class AnimalBasic {
+	private int animalId;
+	private String animalType;
+	private String species;
+	private String age;			// why did you do varchar Hao?
+	private String gender;
+	private String size;		// why did you do varchar Hao?
+	private String url;
+
+}
