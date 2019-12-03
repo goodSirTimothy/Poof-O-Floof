d4dc96a9f3e73c1dcf9f4e4fa38b77a0acca981a
Hao Shen <56559192+HaoSususudio@users.noreply.github.com>
2019-12-02 19:50:43 -0500

    Get New Photo Button now will fetch a random photo url stored in testPhotos.ts (#9)

--- Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
+++ Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../products';
 import { testPhotos } from '../testPhotos';
+import { ConditionalExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-main-photo',
   templateUrl: './main-photo.component.html',
   styleUrls: ['./main-photo.component.css']
 })
-export class MainPhotoComponent {
-  Products = products;
+
+export class MainPhotoComponent implements OnInit {
   Photos = testPhotos;
+  photoNum = this.Photos.length;
+  photoUrl: string;
+
+  getNewPhotoUrl() {
+    const rndPhotoNum = Math.round(Math.random() * (this.photoNum - 1));
+    const photoLargeUrl = this.Photos[rndPhotoNum].large;
+    this.photoUrl = photoLargeUrl;
+  }
+
+  ngOnInit() {
+    this.getNewPhotoUrl();
+  }
+
 }
