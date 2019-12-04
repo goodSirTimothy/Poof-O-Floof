a21bb8e6b957b46c3d2f541814c412bddbdecf8c
Tim C <goodSirTimothy@users.noreply.github.com>
2019-12-03 21:20:51 -0500

    made a function to parse out information from petfinder.com and create a JSON string out of it (while leaving out animals that do not have a photo).

--- Poof-O-Floof/src/main/java/dao/AnimalDao.java
+++ Poof-O-Floof/src/main/java/dao/AnimalDao.java
@@ -1,20 +1,12 @@
 package dao;
 
 import model.AnimalBasic;
-import model.AnimalFull;
 
 public interface AnimalDao {
 
 	/************************************************
 	 * 					Save Logic					*
 	 ***********************************************/
-	/**
-	 * Save full animal to DB? Currently confused with Hao's DB design. I cannot tell what we are keeping or removing.
-	 * @param animalFull
-	 * @return
-	 */
-	boolean saveFullAnimal(AnimalFull animalFull);
-	
 	/**
 	 * save the information of a favorite picture
 	 * @param animalId = the animalID (with this, when can query the DB and retrieve all information
