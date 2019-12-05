9955084669fb8d35fa2337295860c4800c74b3dc
Hao Shen <56559192+HaoSususudio@users.noreply.github.com>
2019-12-05 00:42:18 -0500

    Dev (#24)
    * add services dir and photo-url-provider.service
    
    * Now can import a fix-size batch of 50 photos urls from https://jsonplaceholder.typicode.com/photos?albumId=1. Order of the photo display is radomized.

--- Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
+++ Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { testPhotos } from '../testPhotos';
+import { PhotoUrlProviderService } from '../services/photo-url-provider.service';
 import { ConditionalExpr } from '@angular/compiler';
 
 @Component({
@@ -13,14 +14,45 @@ export class MainPhotoComponent implements OnInit {
   photoNum = this.Photos.length;
   photoUrl: string;
 
+  private photoStreamIndexArray: Array<number>;
+  private photoDisplayIndex = 0;
+  testPhotoUrl: string;
+
+  constructor(private photoUrlProvider: PhotoUrlProviderService) {
+    const photoStreamArraySize = this.photoUrlProvider.getMaxPhotoStreamSize();
+    this.photoStreamIndexArray = [...Array(photoStreamArraySize).keys()];
+    this.shuffle(this.photoStreamIndexArray);
+  }
+
   getNewPhotoUrl() {
     const rndPhotoNum = Math.round(Math.random() * (this.photoNum - 1));
     const photoLargeUrl = this.Photos[rndPhotoNum].large;
     this.photoUrl = photoLargeUrl;
   }
 
+  getTestPhotoUrl() {
+    this.photoUrlProvider.getNextPhotoUrl()
+      .subscribe(
+        data => {
+          console.log(data[this.photoStreamIndexArray[this.photoDisplayIndex]]);
+          this.testPhotoUrl = data[this.photoStreamIndexArray[this.photoDisplayIndex]].url;
+          this.photoDisplayIndex += 1;
+        }
+      );
+  }
+
+  shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+
   ngOnInit() {
     this.getNewPhotoUrl();
+    this.getTestPhotoUrl();
   }
 
 }
--- Poof-O-Floof-ui/src/app/services/photo-url-provider.service.ts
+++ Poof-O-Floof-ui/src/app/services/photo-url-provider.service.ts
@@ -2,13 +2,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable, BehaviorSubject, ReplaySubject, AsyncSubject } from 'rxjs';
 
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class PhotoUrlProviderService {
-  constructor() { }
+  private TEST_PHOTO_BUNDLE_URL = 'https://jsonplaceholder.typicode.com/photos?albumId=1';
+  private PHOTO_BUNDLE_URL = 'http://poof-o-floof/api';
+  private MAX_PHOTO_STREAM_SIZE = 50;
+  private photoStream$ = new ReplaySubject<TestPhotoJSON>(this.MAX_PHOTO_STREAM_SIZE);
+
+  constructor(private http: HttpClient) {
+    this.getPhotoBundle();
+  }
+
+  getMaxPhotoStreamSize() {
+    return this.MAX_PHOTO_STREAM_SIZE;
+  }
+
+  getPhotoBundle() {
+    this.http.get<TestPhotoJSON>(this.TEST_PHOTO_BUNDLE_URL)
+      .subscribe(data => {
+        this.photoStream$.next(data);
+      });
+  }
+
+  getNextPhotoUrl() {
+    return this.photoStream$.asObservable();
+  }
+
 }
 
-export interface photoBundleJSON{
 
+export interface PhotoBundleJSON {
+  animalId: string;
+}
+
+export interface TestPhotoJSON {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
 }
