3ec1452ce25c4f7362a0d601c59da8df9574374d
HaoSususudio <bluehao85@gmail.com>
2019-12-07 16:21:31 -0500

    polished main-photo component. Still needs some work.

--- Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
+++ Poof-O-Floof-ui/src/app/main-photo/main-photo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { testPhotos } from '../testPhotos';
+// import { testPhotos } from '../testPhotos';
 import { PhotoUrlProviderService } from '../services/photo-url-provider.service';
 import { ConditionalExpr } from '@angular/compiler';
 
@@ -10,13 +10,11 @@ import { ConditionalExpr } from '@angular/compiler';
 })
 
 export class MainPhotoComponent implements OnInit {
-  Photos = testPhotos;
-  photoNum = this.Photos.length;
-  photoUrl: string;
 
   private photoStreamIndexArray: Array<number>;
   private photoDisplayIndex = 0;
-  testPhotoUrl: string;
+  private photoStreamIndex = 0;
+  private mainFramePhotoUrl: string;
 
   constructor(private photoUrlProvider: PhotoUrlProviderService) {
     const photoStreamArraySize = this.photoUrlProvider.getMaxPhotoStreamSize();
@@ -24,24 +22,31 @@ export class MainPhotoComponent implements OnInit {
     this.shuffle(this.photoStreamIndexArray);
   }
 
-  getNewPhotoUrl() {
-    const rndPhotoNum = Math.round(Math.random() * (this.photoNum - 1));
-    const photoLargeUrl = this.Photos[rndPhotoNum].large;
-    this.photoUrl = photoLargeUrl;
+  ngOnInit() {
+    // this.getNewPhotoUrl();
+    this.setMainFramePhotoUrl();
   }
 
-  getTestPhotoUrl() {
-    this.photoUrlProvider.getNextPhotoUrl()
+  nextRandomPhoto() {
+    this.setMainFramePhotoUrl();
+    this.photoDisplayIndex += 1;
+  }
+
+  setMainFramePhotoUrl() {
+    this.photoUrlProvider.getPhotoStream()
       .subscribe(
         data => {
-          console.log(data[this.photoStreamIndexArray[this.photoDisplayIndex]]);
-          this.testPhotoUrl = data[this.photoStreamIndexArray[this.photoDisplayIndex]].url;
-          this.photoDisplayIndex += 1;
+          this.photoStreamIndex = this.photoStreamIndexArray[this.photoDisplayIndex];
+          this.mainFramePhotoUrl = data[this.photoStreamIndex].url;
         }
       );
   }
 
-  shuffle(arr) {
+  /**
+   * Fisher–Yates shuffle algorithm, O(n) complexity
+   * @param arr: Array to be shuffled
+   */
+  shuffle(arr: Array<any>) {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [arr[i], arr[j]] = [arr[j], arr[i]];
@@ -49,10 +54,14 @@ export class MainPhotoComponent implements OnInit {
     return arr;
   }
 
+  // Photos = testPhotos;
+  // photoNum = this.Photos.length;
+  // photoUrl: string;
 
-  ngOnInit() {
-    this.getNewPhotoUrl();
-    this.getTestPhotoUrl();
-  }
+  // getNewPhotoUrl() {
+  //   const rndPhotoNum = Math.round(Math.random() * (this.photoNum - 1));
+  //   const photoLargeUrl = this.Photos[rndPhotoNum].large;
+  //   this.photoUrl = photoLargeUrl;
+  // }
 
 }
