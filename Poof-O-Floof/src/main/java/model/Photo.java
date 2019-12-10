edf37ee6cfa59127eeaac48841f4493b8d5a2265
Tim C <goodSirTimothy@users.noreply.github.com>
2019-12-10 10:42:20 -0500

    overhall of the animal sql dao and implemented visitor pattern for sql queries.

--- Poof-O-Floof/src/main/java/model/Photo.java
+++ Poof-O-Floof/src/main/java/model/Photo.java
@@ -3,17 +3,20 @@ package model;
 public class Photo {
 	private int animalId;
 	private int photoId;
-	private String fullUrl;
 	private String type;
+	private String fullUrl;
+	private String url;
 	public Photo() {
+		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Photo(int animalId, int photoId, String fullUrl, String type) {
+	public Photo(int animalId, int photoId, String type, String fullUrl, String url) {
 		super();
 		this.animalId = animalId;
 		this.photoId = photoId;
-		this.fullUrl = fullUrl;
 		this.type = type;
+		this.fullUrl = fullUrl;
+		this.url = url;
 	}
 	public int getAnimalId() {
 		return animalId;
@@ -27,17 +30,23 @@ public class Photo {
 	public void setPhotoId(int photoId) {
 		this.photoId = photoId;
 	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 	public String getFullUrl() {
 		return fullUrl;
 	}
 	public void setFullUrl(String fullUrl) {
 		this.fullUrl = fullUrl;
 	}
-	public String getType() {
-		return type;
+	public String getUrl() {
+		return url;
 	}
-	public void setType(String type) {
-		this.type = type;
+	public void setUrl(String url) {
+		this.url = url;
 	}
 	@Override
 	public int hashCode() {
@@ -47,6 +56,7 @@ public class Photo {
 		result = prime * result + ((fullUrl == null) ? 0 : fullUrl.hashCode());
 		result = prime * result + photoId;
 		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
 		return result;
 	}
 	@Override
@@ -72,11 +82,18 @@ public class Photo {
 				return false;
 		} else if (!type.equals(other.type))
 			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
 		return true;
 	}
 	@Override
 	public String toString() {
-		return "Photo [animalId=" + animalId + ", photoId=" + photoId + ", fullUrl=" + fullUrl + ", type=" + type + "]";
+		return "Photo [animalId=" + animalId + ", photoId=" + photoId + ", type=" + type + ", fullUrl=" + fullUrl
+				+ ", url=" + url + "]";
 	}
+	
 	
 }
