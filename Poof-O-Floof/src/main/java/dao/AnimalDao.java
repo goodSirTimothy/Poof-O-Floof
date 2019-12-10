c05af13c5e4ba6fe81160713ce0f923a409566f2
Tim C <goodSirTimothy@users.noreply.github.com>
2019-12-10 11:11:42 -0500

    overhall of the animal sql dao and implemented visitor pattern for sql queries.

--- Poof-O-Floof/src/main/java/dao/AnimalDao.java
+++ Poof-O-Floof/src/main/java/dao/AnimalDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.util.List;
 
+
 import model.AnimalBasic;
 import model.Photo;
 
@@ -11,30 +12,40 @@ public interface AnimalDao {
 	 * 					Save Logic					*
 	 ***********************************************/
 	/**
-	 * save the information of a favorite picture
-	 * @param animalId = the animalID (with this, when can query the DB and retrieve all information
-	 * @param userId = the user that wanted to save 
+	 * 
+	 * save the all of the information of a photo
+	 * @param userId = the {@link model.User}'s ID that wanted to save the photo
+	 * @param photo = the {@link model.Photo} with all the information
 	 * @return
 	 */
-	
-	boolean savePhoto(Photo photo);
-	
-	boolean saveFavorite(int userId, int photoId);
-	
-	List<Photo> getFavoriteList(int userId);
+	boolean savePhoto(int userId, Photo photo);
 
 	/************************************************
 	 * 					Update Logic				*
 	 ***********************************************/
-	//may need a function to add new column to likes unless it should be handled here
 	
+	//may need a function to add new column to likes unless it should be handled here
 	boolean updateDailyLikes(AnimalBasic animalBasic);
 	
 	//maybe have a function to reset daily likes
-	
 	boolean updateTotalLikes(AnimalBasic animalBasic);
 
 	/************************************************
 	 * 					Get Logic					*
 	 ***********************************************/
+	/**
+	 * 
+	 * @param userId = the {@link model.User}'s ID wanting to view saved photos
+	 * @return
+	 */
+	List<Photo> getFavoriteList(int userId);
+	
+
+	/**
+	 * @deprecated
+	 * @param userId
+	 * @param photoId
+	 * @return
+	 */
+	boolean saveFavorite(int userId, int photoId);
 }
